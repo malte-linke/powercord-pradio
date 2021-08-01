af1842ae0e3b3f3dd52d1d2d48684bb4e85ea6ca
malte-linke <malte.linke.ml@gmail.com>
2021-08-01 15:33:03 +0200

    🔈 tryed to fix mute button v2

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -23,11 +23,11 @@ const Settings = ({ getSetting, updateSetting }) => {
         note="Sets the playback volume"
         required={true}
         minValue={0}
-        maxValue={100}
+        maxValue={50}
         defaultValue={50}
         initialValue={getSetting("stream_volume", 50)}
         markers={[
-          0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
         ]}
         stickToMarkers={true}
         onValueChange={(value) => updateSetting("stream_volume", value)}
