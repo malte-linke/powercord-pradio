6438f2bf68192cf04d16d5cdeca6ee2c724cf00f
malte-linke <malte.linke.ml@gmail.com>
2021-08-01 16:15:48 +0200

    🪄 updated volume control

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -23,11 +23,11 @@ const Settings = ({ getSetting, updateSetting }) => {
         note="Sets the playback volume"
         required={true}
         minValue={0}
-        maxValue={50}
-        defaultValue={50}
-        initialValue={getSetting("stream_volume", 50)}
+        maxValue={25}
+        defaultValue={25}
+        initialValue={getSetting("stream_volume", 10)}
         markers={[
-          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
         ]}
         stickToMarkers={true}
         onValueChange={(value) => updateSetting("stream_volume", value)}
